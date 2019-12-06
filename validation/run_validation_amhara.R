c7007b6f849a53536875717de5dce87f93703f78
michdn <28901045+michdn@users.noreply.github.com>
2019-12-05 18:33:17 -0600

    Added script for running validation, and accompanying Rnw for a formatted validation report

---
+++ validation/run_validation_amhara.R
@@ -0,0 +1,169 @@
+# ###############################################################################################
+#
+# This script runs validation metrics on the EPIDEMIA modeling and forecasting for a 
+# user-specified start week, time range, and n-weeks ahead predictions. 
+#
+# The user must select in Section 1, the starting week, number of weeks, and weeks ahead to
+# run validation (number of weeks to forecast into the future).
+# The user can also assume a delay in reporting with reporting_lag. 
+# Skill test will compare against naive models and generate skill scores. This must be run 
+#     to generate the formatted pdf report.
+#
+# NOTE: Because these are creating forecasts from each week in the validation period,
+#  these will take some time to run. 
+#
+# See documentation/epidemia_howto_run_validation.pdf for more details. 
+#
+# ###############################################################################################
+
+
+# 1. Validation settings -----------------------------------------------------
+
+# This is the week to start the validation from
+date_start <- epidemiar::make_date_yw(year = 2018, week = 1, weekday = 7)
+
+# This is the number of weeks from the start to run validation through.
+total_timesteps <- 52
+
+# This is the number of weeks ahead (1 through this number) to validate.
+timesteps_ahead <- 12
+
+# This is the number of weeks of assumed delay in reporting
+reporting_lag  <- 0
+
+# This will compare against naive, or very simple, models
+# Must be TRUE to generate the formatted pdf reports
+skill_test <- TRUE 
+
+
+# 2. Libraries & Functions ------------------------------------------------------
+
+## Load packages necessary for script
+
+#make sure pacman is installed
+if (!require("pacman")) install.packages("pacman")
+
+#load packages
+#note: data corrals use dplyr, tidyr, lubridate, readr, readxl, epidemiar
+#note: pdf creation requires knitr, tinytex
+pacman::p_load(dplyr,
+               knitr,
+               lubridate,
+               parallel,
+               readr, 
+               readxl,
+               tidyr,
+               tinytex,
+               tools)
+#load specialized package (https://github.com/EcoGRAPH/epidemiar)
+library(epidemiar)
+
+## Locally-defined Functions
+source("R/data_corrals.R") 
+source("R/report_save_create_helpers.R")
+
+
+# 3. Reading in the Data -----------------------------------------------------
+
+# read in woreda metadata
+report_woredas <- readxl::read_xlsx("data/woredas.xlsx", na = "NA") %>% 
+  dplyr::filter(report == 1)
+
+# read & process case data needed for report
+epi_data <- corral_epidemiological(report_woreda_names = report_woredas$woreda_name)
+
+# read & process environmental data for woredas in report
+env_data <- corral_environment(report_woredas = report_woredas)
+
+# read in climatology / environmental reference data
+env_ref_data <- read_csv("data/env_ref_data_2002_2018.csv", col_types = cols())
+
+# read in environmental info file
+env_info <- read_xlsx("data/environ_info.xlsx", na = "NA")
+
+# read in forecast and event detection parameters
+source("data/model_parameters_amhara.R")
+
+
+# 4. Validation P. falciparum ------------------------------------------------------
+
+# Because these are creating forecasts from each week in the validation period,
+#  these will take some time to run. 
+
+# P. falciparum & mixed
+if (exists("epi_data") & exists("env_data")){
+  
+  message("Running P. falciparum & mixed")
+  pfm_validation <- run_validation(date_start = date_start, 
+                                   total_timesteps = total_timesteps, 
+                                   timesteps_ahead = timesteps_ahead, 
+                                   skill_test = skill_test,
+                                   reporting_lag = reporting_lag, 
+                                   #copy of run_epidemia() parameters 
+                                   epi_data = epi_data, 
+                                   casefield = test_pf_tot, 
+                                   populationfield = pop_at_risk,
+                                   inc_per = inc_per,
+                                   groupfield = woreda_name, 
+                                   week_type = "ISO",
+                                   report_period = report_period, 
+                                   ed_summary_period = ed_summary_period,
+                                   ed_method = ed_method, 
+                                   ed_control = pfm_ed_control,
+                                   env_data = env_data, 
+                                   obsfield = environ_var_code, 
+                                   valuefield = obs_value, 
+                                   forecast_future = forecast_future, 
+                                   fc_control = pfm_fc_control,
+                                   env_ref_data = env_ref_data, 
+                                   env_info = env_info,
+                                   model_choice = pfm_model_choice)
+} else {
+  message("Error: Epidemiological and/or environmental datasets are missing.
+          Check Section 3 for data error messages.")
+}
+
+# saves the report data, creates and saves the pdf report
+create_validation_report(pfm_validation)
+
+
+# 5. Validation P. vivax ------------------------------------------------------
+
+# Because these are creating forecasts from each week in the validation period,
+#  these will take some time to run. 
+
+# P. vivax
+if (exists("epi_data") & exists("env_data")){
+  
+  message("Running P. vivax")
+  pv_validation <- run_validation(date_start = date_start, 
+                                  total_timesteps = total_timesteps, 
+                                  timesteps_ahead = timesteps_ahead, 
+                                  skill_test = skill_test,
+                                  reporting_lag = reporting_lag, 
+                                  #copy of run_epidemia() parameters 
+                                  epi_data = epi_data, 
+                                  casefield = test_pv_only, 
+                                  populationfield = pop_at_risk,
+                                  inc_per = inc_per,
+                                  groupfield = woreda_name, 
+                                  week_type = "ISO",
+                                  report_period = report_period, 
+                                  ed_summary_period = ed_summary_period,
+                                  ed_method = ed_method, 
+                                  ed_control = pv_ed_control,
+                                  env_data = env_data, 
+                                  obsfield = environ_var_code, 
+                                  valuefield = obs_value, 
+                                  forecast_future = forecast_future, 
+                                  fc_control = pv_fc_control,
+                                  env_ref_data = env_ref_data, 
+                                  env_info = env_info,
+                                  model_choice = pv_model_choice)
+} else {
+  message("Error: Epidemiological and/or environmental datasets are missing.
+          Check Section 3 for data error messages.")
+}
+
+# saves the report data, creates and saves the pdf report
+create_validation_report(pv_validation)
